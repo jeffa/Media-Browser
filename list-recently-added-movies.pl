e8115fc4b0221acf38295a3fb2656de237d6589d
jeffa <jeffa@unlocalhost.com>
2017-05-30 17:08:13 -0700

    added cli support

--- list-recently-added-movies.pl
+++ list-recently-added-movies.pl
@@ -1,12 +1,55 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Pod::Usage;
+use Getopt::Long;
+use Data::Dumper;
+
 use File::Find::Rule;
 
-my $limit = shift || 19;
-$limit = 19 unless int($limit);
+GetOptions (
+    'dir=s'     => \my $dir,
+    'limit=i'   => \my $limit,
+    'help'      => \my $help,
+    'man'       => \my $man,
+);
 
-my @files = File::Find::Rule->file()->name( '*.avi', '*.mp4', '*.mkv', '*.divx', '*.m4v' )->in( '/Volumes/MOVIES' );
-my @sorted = sort { -M $a <=> -M $b } @files;
+pod2usage( -verbose => 0 ) if $help;
+pod2usage( -verbose => 2 ) if $man;
+pod2usage( 'dir must be valid path' ) unless $dir;
+$limit = 20 unless int($limit);
+
+my @sorted = sort { -M $a <=> -M $b } File::Find::Rule
+    ->file()
+    ->name( '*.avi', '*.mp4', '*.mkv', '*.divx', '*.m4v' )
+    ->in( $dir )
+;
+
 print "$_\n" for @sorted[ 0 .. $limit ];
 
+__END__
+=head1 NAME
+
+list-recently-added-movies - fetch IMDb page(s)
+
+=head1 SYNOPSIS
+
+./list-recently-added-movies --dir=/Volumes/MOVIES --limit=50
+
+ Options:
+   --dir        path to find movies from 
+   --limit      limit number displayed
+   --help       show help
+   --man        print man page
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--dest>
+
+Destination (path) to move files to. Trailing slash will be added if not provided.
+
+  --dest=/Volumes/TV/DOCUMENTARIES
+
+=back
