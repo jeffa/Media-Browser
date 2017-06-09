5cfae6c6de0f86862781012c4be2b582cac48ed1
jeffa <jeffa@unlocalhost.com>
2017-06-09 14:58:45 -0700

    extract ready

--- extract-file-info.pl
+++ extract-file-info.pl
@@ -36,27 +36,41 @@ if ($dir) {
 
 for my $file (@files) {
     my %rel;
-    my $xml = `mediainfo -f --Output=XML '$file'`;
-    my $media = XMLin( $xml, ForceArray => [] );
+    my $xml = `mediainfo -f --Output=XML "$file"`;
+
+    my $media;
+    eval { $media = XMLin( $xml, ForceArray => [] ) };
+    if ($@) {
+        warn "Could not parse $file: $@\n";
+        next;
+    }
+
     my ($header,$video,$audio) = @{ $media->{File}{track} };
 
     $rel{duration}          = $header->{Duration}[4];
     $rel{file_name}         = $header->{Complete_name};
-    $rel{bit_rate}          = $header->{Overall_bit_rate}[0];
     $rel{file_size}         = $header->{File_size}[0];
     $rel{display_size}      = $header->{File_size}[1];
+
     $rel{source}            = $video->{Movie_name} || $video->{Title} || $video->{Comment};
     $rel{frame_rate}        = $video->{Frame_rate}[0];
+    $rel{bit_rate}          = $video->{Bit_rate}[0];
     $rel{codec}             = $video->{Codec}[0];
     $rel{width}             = $video->{Width}[0];
     $rel{height}            = $video->{Height}[0];
     $rel{ratio}             = $video->{Display_aspect_ratio}[1];
+    $rel{scan_type}         = $video->{Scan_type}[0];
+    $rel{format}            = $video->{Format};
+    $rel{version}           = $video->{Encoded_Library_Version};
+
     $rel{audio_format}      = $audio->{Format};
     $rel{audio_sample_rate} = $audio->{Sampling_rate}[0];
     $rel{audio_bit_rate}    = $audio->{Bit_rate}[0];
+    $rel{audio_bit_mode}    = $audio->{Bit_rate_mode}[0];
     $rel{audio_channels}    = $audio->{Channel_s_}[0];
 
-    print encode_json( \%rel ), $/;
+    #print encode_json( \%rel ), $/;
+    print Dumper \%rel;
 }
 
 __END__
