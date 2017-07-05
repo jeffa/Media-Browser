5a31fee179776c3c6b4159d3a93e38c8a9414d84
jeffa <jeffa@unlocalhost.com>
2017-07-05 13:29:11 -0400

    can edit tags

--- display-webapp.pl
+++ display-webapp.pl
@@ -29,20 +29,76 @@ get '/tags' => sub {
     my $self = shift;
     my $title_id = int( $self->param( 'title_id' ) || 0 );
     my $edit     = int( $self->param( 'edit' ) || 0 );
-    my $tags     = lc( $self->param( 'tags' ) || '' );
+    my $new_tags = lc( $self->param( 'tags' ) || '' );
 
-    my $tags = $dbh->selectcol_arrayref(
-        'SELECT tag FROM tags INNER JOIN tag_xref x ON tags.tag_id=x.tag_id WHERE x.title_id = ?',
-        undef, $title_id
-    );
+    my @tags = tags( $dbh, $title_id );
+
+    unless ($edit) {
+        $self->stash(
+            title_id    => $title_id,
+            tags        => @tags ? \@tags : [ 'add tags' ],
+            joined_tags => join( ' ', @tags ),
+        );
+        $self->render( template => 'show-tags' );
+        return;
+    }
+
+    my %old = map { $_ => 1 } @tags;
+    my %new = map { $_ => 1 } split( ' ', $new_tags );;
+
+    my $sth = $dbh->selectall_arrayref('select tag, tag_id from tags');
+    my %all = map { @$_ } @$sth;
+
+    my @to_add = map { $old{$_} ? () : $all{$_} || $_ } keys %new;
+    my @tag_ids;
+    for my $tag (@to_add) {
+        if ($tag =~ /\D/) {
+            $dbh->do( 'insert into tags(tag) values(?)', undef, $tag );
+            push @tag_ids, $dbh->{mysql_insertid};
+        } else {
+            push @tag_ids, $tag;
+        }
+    }
+
+    for my $tag_id (@tag_ids) {
+        $dbh->do( 'insert into tag_xref(tag_id,title_id) values(?,?)', undef, $tag_id, $title_id );
+    }
+
+    my @to_unlink = map { $new{$_} ? () : $all{$_} } keys %old;
+
+    my @to_delete;
+    @to_delete = @{ $dbh->selectcol_arrayref( '
+        select x.tag_id, count(*) as count
+        from tag_xref x 
+        inner join tags t on x.tag_id=t.tag_id 
+        where x.tag_id in( ' . join(', ', @to_unlink ) . ')
+        group by x.tag_id
+        having count = 1
+    ') } if @to_unlink;
+
+    if (@to_delete) {
+        $dbh->do(
+            'delete from tags where tag_id in( ' 
+            . join(', ', @to_delete ) . ')'
+        );
+    }
+
+    if (@to_unlink) {
+        $dbh->do( 
+            'delete from tag_xref where title_id = ? and tag_id in( '
+            . join(', ', @to_unlink ) . ')',
+            undef, $title_id
+        );
+    }
+
+    my @tags = tags( $dbh, $title_id );
 
     $self->stash(
         title_id    => $title_id,
-        tags        => @$tags ? $tags : [ 'add tags' ],
-        joined_tags => join( ' ', @$tags ),
+        tags        => \@tags,
+        joined_tags => join( ' ', @tags ),
     );
-
-    $self->render( template => $edit ? 'edit-tags' : 'show-tags' );
+    $self->render( template => 'edit-tags' );
 };
 
 get '/fetch' => sub {
@@ -266,6 +322,17 @@ get '/fetch' => sub {
 
 app->start( daemon => '-l', 'http://*:3000' );
 
+sub tags {
+    my ($dbh, $title_id) = @_;
+    @{ $dbh->selectcol_arrayref('
+        select t.tag
+        from tags t
+        inner join tag_xref x
+        on t.tag_id=x.tag_id
+        where x.title_id = ?
+        order by t.tag
+    ', undef, $title_id ) }
+}
 
 
 __DATA__
@@ -531,14 +598,22 @@ function show_tag( title_id, input ) {
                 <td>
                     <div id="tag-<%= $obj->{title_id} %>" width="100%">
                     <% for (@{$obj->{tags}}) { %>
+                      <% if ($DEBUG) { %>
                         <span class="badge alert-success" onclick="javascript: edit_tag( <%= $obj->{title_id} %>, '<%= join( ' ', map $_->{tag}, @{$obj->{tags}} ) %>' )">
                             <%= $_->{tag} %>
                         </span>
+                      <% } else { %>
+                        <span class="badge alert-success"><%= link_to $_->{tag} => "javascript: by_link( 'tag', '$_->{tag_id}' )" %></span>
+                      <% } %>
                     <% } %>
                     <% unless (@{ $obj->{tags} || {} }) { %>
+                      <% if ($DEBUG) { %>
                         <span class="badge alert-success" onclick="javascript: edit_tag( <%= $obj->{title_id} %>, '' )">
                             add tags <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
                         </span>
+                      <% } else { %>
+                        <span class="badge alert-success"><%= link_to $_->{tag} => "javascript: by_link( 'tag', '$_->{tag_id}' )" %></span>
+                      <% } %>
                     <% } %>
                     </div>
                 </td>
